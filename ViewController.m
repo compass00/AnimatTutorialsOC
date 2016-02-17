4669e74c84de630f3148cfb17d5da75981dc6640
LM <287846224@qq.com>
2016-02-17 18:20:54 +0800

    添加返回动画。

--- ViewController.m
+++ ViewController.m
@@ -9,13 +9,14 @@
 #import "ViewController.h"
 
 @interface ViewController ()
-
+@property BOOL run;
 @end
 
 @implementation ViewController
 
 - (void)viewDidLoad {
     [super viewDidLoad];
+    _run = NO;
     // Do any additional setup after loading the view, typically from a nib.
 }
 
@@ -26,21 +27,34 @@
 
 - (IBAction)playAnimation:(id)sender {
     const CGFloat offset = CGRectGetWidth([UIScreen mainScreen].bounds);
-    [UIView animateWithDuration: 0.5 animations: ^{
-        self.carblue.center = CGPointMake(offset - self.carblue.frame.size.width / 2, self.carblue.center.y);
-    } completion: ^(BOOL finished){
-        [UIView animateWithDuration: 0.5 delay: 0.01 options: 0 animations: ^{
-        
-            self.cargreen.center = CGPointMake(offset - self.cargreen.frame.size.width / 2, self.cargreen.center.y);
-        } completion: ^(BOOL finished) {
-        /*    [UIView animateWithDuration: 1.5 delay: 0 usingSpringWithDamping: 0.1 initialSpringVelocity: 0 options: 0 animations: ^{
-                self.login.alpha = 1;
-                CGPoint center = self.login.center;
-                center.y -= 100;
-                self.login.center = center;
-            } completion: nil];*/
+    if (!_run) {
+        [UIView animateWithDuration: 0.5 animations: ^{
+            self.carblue.center = CGPointMake(offset - self.carblue.frame.size.width / 2, self.carblue.center.y);
+        } completion: ^(BOOL finished){
+            [UIView animateWithDuration: 0.5 delay: 0.01 options: 0 animations: ^{
+                
+                self.cargreen.center = CGPointMake(offset - self.cargreen.frame.size.width / 2, self.cargreen.center.y);
+            } completion: ^(BOOL finished) {
+                [UIView animateWithDuration: 1.5 delay: 0 usingSpringWithDamping: 0.6 initialSpringVelocity: 0 options: 0 animations: ^{
+                    self.carper.center = CGPointMake(offset - self.carper.frame.size.width / 2, self.carper.center.y);
+                } completion: nil];
+            }];
         }];
-    }];
+    } else {
+        [UIView animateWithDuration: 0.5 animations: ^{
+            self.carblue.center = CGPointMake(self.carblue.frame.size.width + 10, self.carblue.center.y);
+        } completion: ^(BOOL finished){
+            [UIView animateWithDuration: 0.5 delay: 0.01 options: 0 animations: ^{
+                
+                self.cargreen.center = CGPointMake(self.cargreen.frame.size.width + 10, self.cargreen.center.y);
+            } completion: ^(BOOL finished) {
+                [UIView animateWithDuration: 1.5 delay: 0 usingSpringWithDamping: 0.6 initialSpringVelocity: 0 options: 0 animations: ^{
+                    self.carper.center = CGPointMake(self.carper.frame.size.width + 10, self.carper.center.y);
+                } completion: nil];
+            }];
+        }];
+    }
+    _run = !_run;
     
 }
 
